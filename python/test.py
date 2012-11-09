482a1938efeec4c9d3af69e1a25473264f2af71a
Greg Hogan <code@greghogan.com>
2012-11-09 12:10:26 -0800

    Problem 2
    New solution.

---
+++ python/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Run doctests on all local modules.
+"""
+
+
+import doctest
+import glob
+import os
+
+# Project Euler program names begin with a capital letter
+for python_file in glob.glob(os.path.dirname(__file__) + '/[A-Z]*.py'):
+    name = os.path.basename(python_file)[:-3]
+    module = __import__(name, globals(), locals(), [name])
+    doctest.testmod(module)
