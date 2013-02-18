4322caa4f604eaea3911a74a67443426ae0ab1fb
Greg Hogan <code@greghogan.com>
2013-02-18 14:17:11 -0800

    Problem 1
    Updated solution to solve problem in the general case.

--- python/A1.py
+++ python/A1.py
@@ -10,18 +10,81 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-def naive():
-    """Compute sum of multiples.  This runs in linear time.
+import itertools
 
-    >>> naive()
+def gcd(*integers):
+    """Compute the Greatest Common Divisor of a list of integers.
+
+    >>> gcd(5, 0)
+    5
+    >>> gcd(19, 21)
+    1
+    >>> gcd(4, 6, 8)
+    2
+    >>> gcd(10, 15, 20)
+    5
+    """
+
+    def _gcd(a, b):
+        while b != 0:
+            a, b = b, a % b
+
+        return a
+
+    if any(i < 0 for i in integers):
+        raise Exception('GCD input must be non-negative integers')
+
+    return reduce(_gcd, integers)
+
+
+def lcm(*integers):
+    """Compute the Least Common Multiple of a list of integers.
+
+    >>> lcm(5, 0)
+    0
+    >>> lcm(19, 21)
+    399
+    >>> lcm(4, 6, 8)
+    24
+    """
+
+    def _lcm(a, b):
+        return a * (b / gcd(a, b))
+
+    if any(i < 0 for i in integers):
+        raise Exception('LCM input must be non-negative integers')
+
+    return reduce(_lcm, integers)
+
+
+def naive(n, factors):
+    """Compute the sum of numbers less than n which are multiples of factors.
+    This runs in linear time.
+
+    >>> naive(10, [3, 5])
+    23
+    >>> direct(10**5, [4, 6])
+    1666583334
+    >>> naive(10**5, [2, 3, 5])
+    3666683332
+    >>> naive(10**5, [2, 3, 5, 7])
+    3857107139
+    >>> naive(10**5, [7, 13, 17, 29, 31])
+    1520917441
+    >>> naive(10**5, range(14, 20))
+    1507660080
+    >>> naive(10**5, [3, 6, 9])
+    1666683333
+    >>> naive(1000, [3, 5])
     233168
     """
 
-    return sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0)
+    return sum(x for x in range(n) if any(x % f == 0 for f in factors))
 
 
-def quick():
-    """Compute the sum of multiples in constant time.
+def direct(n, factors):
+    """Compute the sum of numbers less than n which are multiples of factors.
+    This runs in constant time.
 
     The multiples of 3 below 1000 are 3, 6, 9, ... 996, 999.
     The multiples of 5 below 1000 are 5, 10, 15, ... 990, 995.
@@ -29,7 +92,21 @@ def quick():
 
     The sum of multiples of n below N is n*p*(p+1)/2 where p=(N-1)/n
 
-    >>> quick()
+    >>> direct(10, [3, 5])
+    23
+    >>> direct(10**5, [4, 6])
+    1666583334
+    >>> direct(10**5, [2, 3, 5])
+    3666683332
+    >>> direct(10**5, [2, 3, 5, 7])
+    3857107139
+    >>> direct(10**5, [7, 13, 17, 29, 31])
+    1520917441
+    >>> direct(10**5, range(14, 20))
+    1507660080
+    >>> direct(10**5, [3, 6, 9])
+    1666683333
+    >>> direct(1000, [3, 5])
     233168
     """
 
@@ -37,7 +114,13 @@ def quick():
         p = (N-1) // n
         return n * p * (p+1) / 2
 
-    return sum_of_multiples(1000, 3) + sum_of_multiples(1000, 5) - sum_of_multiples(1000, 15)
+    # Remove factors which are evenly divisible by another factor; this is an optimization and not required
+    f = [x for x in factors if not any(x != y and x % y == 0 for y in factors)]
+
+    # The inclusion-exclusion principle:  sum the multiples of each factor,
+    # then substract the sum of multiples of the LCM of each pair of factors,
+    # then add the sum of multiples of the LCM of each triplet of factors, ...
+    return sum((1 if k % 2 else -1) * sum(sum_of_multiples(n, lcm(*c)) for c in itertools.combinations(f, k)) for k in range(1, len(f) + 1))
 
 
 if __name__ == "__main__":
