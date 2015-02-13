1874de3e8cc1a5057d930f8c223e5da951946e3e
Greg Hogan <code@greghogan.com>
2015-02-13 06:43:17 -0500

    Licensing
    Code now licensed under GPLv3 and proper attribution to Project Euler.

--- python/A1.py
+++ python/A1.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
+# Copyright Greg Hogan, 2012. Last modified 2015.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but without any warranty; without even the implied warranty of
+# merchantability or fitness for a particular purpose.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
+The following problem is taken from Project Euler,
+    https://projecteuler.net/problem=1
+licenced under a Creative Commons Licence:
+    Attribution-NonCommercial-ShareAlike 2.0 UK: England & Wales
+
 Problem 1
 05 October 2001
 
